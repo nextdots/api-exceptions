e603ed6140328ae515f0ba180142b9673682dab0
Franco Correa <anibalfranciscocorrea@gmail.com>
2017-01-18 13:31:04 -0300

    Fix psr-2 style code

--- src/Handler.php
+++ src/Handler.php
@@ -2,21 +2,18 @@
 
 namespace ApiExceptions;
 
-use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use Exception;
-use Illuminate\Http\Response;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\Exception\HttpResponseException;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\Debug\Exception\FlattenException;
-
-use ApiExceptions\JsonResponseException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
     /**
-     * The errors.
+     * The errors in the exception.
      *
      * @var array
      */
@@ -29,46 +26,39 @@ class Handler extends ExceptionHandler
      */
     private $description;
 
-
+    /**
+     * Handler constructor.
+     */
     public function __construct()
     {
-      $this->errors = [];
+        $this->errors = [];
 
-      $this->description = null;
+        $this->description = null;
     }
 
     public function render($request, Exception $e)
     {
-        if ($e instanceof JsonResponseException)
-        {
+        if ($e instanceof JsonResponseException) {
             return $e->getJsonResponse();
         }
 
         // create json from exception
         $content = $e->getMessage();
-        if ($e instanceof HttpResponseException)
-        {
+
+        if ($e instanceof HttpResponseException) {
             $code = 400;
-        }
-        elseif ($e instanceof ModelNotFoundException)
-        {
+        } elseif ($e instanceof ModelNotFoundException) {
             $code = 404;
-        }
-        elseif ($e instanceof AuthorizationException)
-        {
+        } elseif ($e instanceof AuthorizationException) {
             $code = 403;
-        }
-        elseif ($e instanceof ValidationException && $e->getResponse())
-        {
+        } elseif ($e instanceof ValidationException && $e->getResponse()) {
             $errors = json_decode($e->getResponse()->getContent());
             $this->errors = json_decode(json_encode($errors), true);
             $code = 422;
         }
         // add more cases here
-        else
-        {
-            switch (FlattenException::create($e)->getStatusCode())
-            {
+        else {
+            switch (FlattenException::create($e)->getStatusCode()) {
                 case 404:
                     $code = 404;
                     $content = 'Sorry, the page you are looking for could not be found';
